66bd34ec68514e16fb1b90556fb6624577a647aa
Andrej Mihajlov <and@codeispoetry.ru>
2014-04-29 22:03:58 +0200

    Document UIActionSheet+Blocks

--- UIActionSheet+Blocks.h
+++ UIActionSheet+Blocks.h
@@ -10,9 +10,37 @@
 
 @interface UIActionSheet (Blocks)
 
+/**
+ *  Alternative initializer for UIActionSheet
+ *
+ *  @param title an action sheet title
+ *
+ *  @return an instance of UIActionSheet or nil
+ */
 - (id)initWithTitle:(NSString*)title;
+
+/**
+ *  Add button with block handler
+ *
+ *  @param title a button title
+ *  @param block a handler block
+ */
 - (void)addButtonWithTitle:(NSString*)title block:(void(^)(void))block;
+
+/**
+ *  Add cancel button with block handler
+ *
+ *  @param title a cancel button title
+ *  @param block a handler block
+ */
 - (void)addCancelButtonWithTitle:(NSString*)title block:(void(^)(void))block;
+
+/**
+ *  Add destructive button with block handler
+ *
+ *  @param title a destructive button title
+ *  @param block a handler block
+ */
 - (void)addDestructiveButtonWithTitle:(NSString*)title block:(void(^)(void))block;
 
 @end
